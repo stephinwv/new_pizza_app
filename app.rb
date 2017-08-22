b84e2eed0cb9eea1e1708a4b0131ca8712588ced
stephinwv <thekingdomseeker@aol.com>
2017-08-22 18:47:53 -0400

    Add files via upload
    Making some progress.....

--- app.rb
+++ app.rb
@@ -1,10 +1,37 @@
 require "sinatra"
-
+enable :sessions
 get '/' do
 	erb :index
 end
-post '/selection' do
-	
+post '/index' do
+  	session[:meats] = params[:meats]
+ 	session[:cheese] = params[:cheese]
+ 	session[:sauce] = params[:sauce]
+ 	session[:veggies] = params[:veggies]
+ 	redirect '/selection'
 end
+ 	
+get '/selection' do
+
+  	erb :selection, :locals=>{cheese: cheese, meats: meats, sauce: sauce, veggies: veggies}
+end
+
+post '/selection' do
+  	session[:meats] = params[:meats]
+ 	session[:cheese] = params[:cheese]
+ 	session[:sauce] = params[:sauce]
+ 	session[:veggies] = params[:veggies]
+    session[:toppings] = params[:toppings]
+    redirect'/results' 
+end  
+get '/results' do
+	session[:meats] = params[:meats]
+ 	session[:cheese] = params[:cheese]
+ 	session[:sauce] = params[:sauce]
+ 	session[:veggies] = params[:veggies]
+    session[:toppings] = params[:toppings]
+	erb :results
+end	
+
 
 	
