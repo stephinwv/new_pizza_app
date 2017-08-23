96e09127b982d63e90f62c4964d2290f701df109
stephinwv <thekingdomseeker@aol.com>
2017-08-23 06:37:44 -0400

    Add files via upload
    For some dumb reason, the second page doesn't display. The variables carry, they printed in the console. There is literally no view of the second page, only black view

--- app.rb
+++ app.rb
@@ -1,37 +1,40 @@
 require "sinatra"
 enable :sessions
+
 get '/' do
 	erb :index
 end
 post '/index' do
-  	session[:meats] = params[:meats]
- 	session[:cheese] = params[:cheese]
- 	session[:sauce] = params[:sauce]
- 	session[:veggies] = params[:veggies]
- 	redirect '/selection'
+	puts "MADE IT TO POST INDEX"
+  	session[:meatstype]= params[:meats]
+ 	session[:cheesetype] = params[:cheese]
+ 	session[:saucetype] = params[:sauce]
+ 	session[:veggietype] = params[:veggies]
+ 	redirect '/selection' 
+ puts" POST INDEX #{meats} #{cheese} #{sauce} #{veggies}"
 end
  	
 get '/selection' do
-
-  	erb :selection, :locals=>{cheese: cheese, meats: meats, sauce: sauce, veggies: veggies}
+	
+puts " GET SELECTION #{meats} #{cheese} #{sauce} #{veggies}"
+  	erb :selection, locals:{cheese: session[:cheesetype], meats:session[:meatstype], sauce: sessions[:saucetype], veggies: sessions[:veggies]}
 end
 
+
 post '/selection' do
-  	session[:meats] = params[:meats]
- 	session[:cheese] = params[:cheese]
- 	session[:sauce] = params[:sauce]
- 	session[:veggies] = params[:veggies]
-    session[:toppings] = params[:toppings]
-    redirect'/results' 
+	puts'MADE IT TO POST SELECTION'
+  	meats = params[:meats]
+ 	cheese = params[:cheese]
+ 	sauce = params[:sauce]
+ 	veggies = params[:veggies]
+ 	puts "meats = #{meats}, cheese = #{cheese} sauce = #{sauce} veggies = #{veggies}"
+    # toppings = params[:toppings]
+    # redirect'/results'
 end  
-get '/results' do
-	session[:meats] = params[:meats]
- 	session[:cheese] = params[:cheese]
- 	session[:sauce] = params[:sauce]
- 	session[:veggies] = params[:veggies]
-    session[:toppings] = params[:toppings]
-	erb :results
-end	
+# get '/results' do
+#  	toppings = params[:toppings].split(',')
+# 	erb :results, :locals=>{  toppings: toppings}
+# end	
 
 
 	
