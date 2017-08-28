140a66c0fbb92fbf646ff0dc36b6e94f9c50c08d
stephinwv <thekingdomseeker@aol.com>
2017-08-28 13:41:44 -0400

    added crust and size options that I forgot to add before, and I added an option to build another pizza but I haven't found out how to save the info.

--- app.rb
+++ app.rb
@@ -6,25 +6,28 @@ get '/' do
 end
 post '/index' do
 	puts "MADE IT TO POST INDEX"
+	session[:size] = params[:size]
+	session[:crust] = params[:crust]
   	session[:meatstype]= params[:meats]
  	session[:cheesetype] = params[:cheese]
- 	session[:saucetype] = params[:sauce]
+ 	session[:sauce] = params[:sauce]
  	session[:veggietype] = params[:veggies]
  	redirect '/selection' 
 end
  	
 get '/selection' do
 	confirmed = params[:confirmed]
-	erb :selection, locals: {meats: session[:meatstype], cheese: session[:cheesetype], sauce: session[:saucetype], veggies: session[:veggietype], confirmed:confirmed} 	
+	erb :selection, locals: {size: session[:size], crust: session[:crust], meats: session[:meatstype], cheese: session[:cheesetype], sauce: session[:sauce], veggies: session[:veggietype], confirmed:confirmed} 	
 
 end
 post '/selection' do
 	session[:confirmed] = params[:confirmed]
 	puts "THIS IS CONFIRMED"
 	puts session[:confirmed]
+	
 	session[:lastname] = params[:lastname]
 	session[:delivery] = params[:delivery]
-	session[:street] = params[:street]
+	session[:street] = params[:street]        
 	session[:city] = params[:city]
 	puts " #{session[:confirmed] }"
     redirect'/results'
@@ -33,4 +36,20 @@ get '/results' do
 
 	
 erb :results, locals: {confirmed: session[:confirmed], delivery: session[:delivery], lastname:session[:lastname], street:session[:street], city: session[:city]}
+
+end
+post '/results' do
+session[:confirmed] = params[:confirmed]
+	puts "THIS IS CONFIRMED"
+	puts session[:confirmed]
+	
+	session[:lastname] = params[:lastname]
+	session[:delivery] = params[:delivery]
+	session[:street] = params[:street]        
+	session[:city] = params[:city]
+	puts " #{session[:confirmed] }"
+    redirect'/index'
+end  
+get '/index' do
+	erb :index
 end
